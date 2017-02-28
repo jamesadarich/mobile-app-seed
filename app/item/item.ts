3d33d288ba319fbdf916536a8288a48692a98a3f
jamesrichford <jamesrichford@googlemail.com>
2017-02-28 13:39:16 +0000

    lots more tinkering

--- app/item/item.ts
+++ app/item/item.ts
@@ -1,3 +1,14 @@
+import "tns-platform-declarations/android.d";
+
+export class Receiver extends android.content.BroadcastReceiver {
+    public onReceive(context: android.content.Context, intent: android.content.Intent) {
+        const serviceIntent = new android.content.Intent(context, Receiver.class);
+        serviceIntent.putExtra("something", intent.getSerializableExtra(""));
+        context.startService(serviceIntent);
+
+    }
+}
+
 export class Item {
     id: number;
     name: string;
